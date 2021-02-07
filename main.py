bfa0d5467ce1d30adae1b182429ac0d346023645
wkostuch <wkostuch@gmail.com>
2021-02-06 22:27:29 -0600

    Add main file for running the simulation.

---
+++ main.py
@@ -0,0 +1,38 @@
+"""
+Authors: Therese Aglialoro, Cameron Nottingham, and William Kostuch
+2021 MCM, Problem A: Fungi
+"""
+import matplotlib.pyplot as plt 
+import numpy as np
+
+from world import World
+
+
+CLIMATE_NAMES = ["Desert", "Tundra", "Grassland", "Shrubland",  
+                "TemperateDeciduousForest", "ConiferousForest",
+                "Rainforest"]
+
+FUNGUS_NAMES = []
+
+
+if __name__ == "__main__":
+    worlds = []
+    for climate in CLIMATE_NAMES:
+        # Arrays for holding values to graph
+        time_array = np.zeros(365)
+        temp_array = np.zeros(365)
+
+        # Make a World and run it for a year
+        world = World(climate, (100, 100), [])
+        for i in range(365):
+            time = world.get_time()
+            time_array[time] = time 
+            temp_array[time] = world.get_environment().get_climate().get_climate_temperature()
+            world.increment_time()
+        plt.plot(time_array, temp_array, label=climate)
+    plt.legend()
+    plt.xlabel("Time (days)")
+    plt.ylabel("Temperature (degrees C)")
+    plt.show()
+
+        
